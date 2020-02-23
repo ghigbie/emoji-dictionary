04be1e0d74f08040a237801e3d3b5e32604e98c8
ghigbie <gehigbie@gmail.com>
2020-02-23 12:01:34 -0700

    Inflate layout

--- app/src/main/java/com/traiingtrack/emojidictionary/EmojiAdapter.kt
+++ app/src/main/java/com/traiingtrack/emojidictionary/EmojiAdapter.kt
@@ -1,5 +1,6 @@
 package com.traiingtrack.emojidictionary
 
+import android.view.LayoutInflater
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
@@ -7,7 +8,7 @@ import android.view.ViewGroup
 class EmojiAdapter: RecyclerView.Adapter<EmojiAdapter.TextHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return TextHolder(LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item_row, parent, false))
     }
 
     override fun getItemCount(): Int {
