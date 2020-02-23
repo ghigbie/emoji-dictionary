81033190c17a98831f123c1211cc46291635a106
ghigbie <gehigbie@gmail.com>
2020-02-23 11:48:24 -0700

    Edit class

--- app/src/main/java/com/traiingtrack/emojidictionary/EmojiAdapter.kt
+++ app/src/main/java/com/traiingtrack/emojidictionary/EmojiAdapter.kt
@@ -1,4 +1,27 @@
 package com.traiingtrack.emojidictionary
 
-class EmojiAdapter {
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class EmojiAdapter: RecyclerView.Adapter<EmojiAdapter.TextHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: TextHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class TextHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener{
+        override fun onClick(v: View?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
 }
