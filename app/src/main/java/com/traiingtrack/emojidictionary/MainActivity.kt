8a384d583a6ee6dfccee87deec273191b499718b
ghigbie <gehigbie@gmail.com>
2020-02-23 11:31:46 -0700

    Add new configuration

--- app/src/main/java/com/traiingtrack/emojidictionary/MainActivity.kt
+++ app/src/main/java/com/traiingtrack/emojidictionary/MainActivity.kt
@@ -2,11 +2,20 @@ package com.traiingtrack.emojidictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var layoutManager: LinearLayoutManager
+    lateinit var recyclerView :RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
     }
 }
