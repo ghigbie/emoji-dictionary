adcfae07e74ae8b8105f29597f2400a7f26491e3
ghigbie <gehigbie@gmail.com>
2020-02-23 19:26:36 -0700

    Change linear layout to grid layout

--- app/src/main/java/com/traiingtrack/emojidictionary/MainActivity.kt
+++ app/src/main/java/com/traiingtrack/emojidictionary/MainActivity.kt
@@ -2,19 +2,20 @@ package com.traiingtrack.emojidictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var layoutManager: LinearLayoutManager
+    lateinit var layoutManager: GridLayoutManager
     lateinit var adapter: EmojiAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        layoutManager = LinearLayoutManager(this)
+        layoutManager = GridLayoutManager(this, 3)
         recyler_view.layoutManager = layoutManager
 
         val emojis = arrayListOf<String>(
